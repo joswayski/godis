0f0876c522a6017dcb4172693c9173ba3a388f23
Jose Valerio <contact@josevalerio.com>
2026-03-12 09:12:08 -0400

    images init

--- internal/godis/replies.go
+++ internal/godis/replies.go
@@ -1,8 +1,10 @@
 package godis
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -55,6 +57,7 @@ func (g *Godis) HandleReplies(s *discordgo.Session, m *discordgo.MessageCreate) 
 		// https://openrouter.ai/docs/quickstart
 		Model:               g.Config.AIApiModels[0], // TODO allow fallbacks / retries,
 		MaxCompletionTokens: openai.Int(int64(g.Config.AIMaxOutputTokens)),
+		Modalities:          []string{"text", "image"},
 	}
 
 	var messages []openai.ChatCompletionMessageParamUnion
@@ -97,7 +100,16 @@ func (g *Godis) HandleReplies(s *discordgo.Session, m *discordgo.MessageCreate) 
 	jitter := time.Duration(rand.IntN(2000)+100) * time.Millisecond
 	time.Sleep(jitter)
 
-	_, err = s.ChannelMessageSend(m.ChannelID, response.Choices[0].Message.Content)
+	imageFiles := extractImages(response.Choices[0].Message.RawJSON())
+	if len(imageFiles) > 0 {
+		_, err = s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+			Content: response.Choices[0].Message.Content,
+			Files:   imageFiles,
+		})
+	} else {
+		_, err = s.ChannelMessageSend(m.ChannelID, response.Choices[0].Message.Content)
+
+	}
 
 	if err != nil {
 		slog.Error("Error sending discord message", "response", response)
@@ -191,4 +203,50 @@ func downloadFile(url string) ([]byte, error) {
 	}
 
 	return io.ReadAll(resp.Body)
+}
+
+type openRouterImage struct {
+	Type     string `json:"type"`
+	ImageURL struct {
+		URL string `json:"url"`
+	} `json:"image_url"`
+}
+
+func extractImages(rawJSON string) []*discordgo.File {
+	var parsed struct {
+		Images []openRouterImage `json:"images"`
+	}
+
+	err := json.Unmarshal([]byte(rawJSON), &parsed)
+
+	if err != nil {
+		return nil
+	}
+
+	var files []*discordgo.File
+	for i, img := range parsed.Images {
+		parts := strings.SplitN(img.ImageURL.URL, ",", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		data, err := base64.StdEncoding.DecodeString(parts[1])
+		if err != nil {
+			slog.Error("Error decoding image", "error", err.Error())
+			continue
+		}
+
+		ext := "png"
+		if strings.Contains(parts[0], "image/jpeg") {
+			ext = "jpeg"
+		}
+
+		files = append(files, &discordgo.File{
+			Name:        fmt.Sprintf("image_%d.%s", i, ext),
+			ContentType: fmt.Sprintf("image/%s", ext),
+			Reader:      bytes.NewReader(data),
+		})
+	}
+
+	return files
 }
