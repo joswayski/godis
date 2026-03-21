9b614bdfe658b0367c3a6fece4195797825013ad
Jose Valerio <contact@josevalerio.com>
2026-03-21 17:20:39 -0400

    init

--- internal/godis/godis.go
+++ internal/godis/godis.go
@@ -1,14 +1,23 @@
 package godis
 
 import (
+	"sync"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/joswayski/godis/internal/config"
 	"github.com/openai/openai-go/v3"
 )
 
+type MessageBuffer struct {
+	BufferedMessages []*discordgo.Message
+	Timer            *time.Timer
+}
 type Godis struct {
-	Config   config.GodisConfig
-	AIClient openai.Client
+	Config        config.GodisConfig
+	AIClient      openai.Client
+	mutex         sync.Mutex
+	MessageBuffer map[string]MessageBuffer // String is the channel ID
 }
 
 func (g *Godis) HandleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
--- internal/godis/replies.go
+++ internal/godis/replies.go
@@ -8,6 +8,7 @@ import (
 	"log/slog"
 	"math/rand/v2"
 	"net/http"
+	"slices"
 	"strings"
 	"time"
 
@@ -16,8 +17,9 @@ import (
 	"github.com/openai/openai-go/v3"
 )
 
-// https://developers.openai.com/api/reference/go/
+const maxBufferedMessages = 5
 
+// https://developers.openai.com/api/reference/go/
 func (g *Godis) HandleReplies(s *discordgo.Session, m *discordgo.MessageCreate) {
 	slog.Info("Message handling in replies", "message", m)
 
@@ -38,70 +40,32 @@ func (g *Godis) HandleReplies(s *discordgo.Session, m *discordgo.MessageCreate) 
 		return
 	}
 
-	// Sometimes messages don't have embeds populated right away
-	shouldRefetch := shouldRefetchForEmbeds(m.Message)
-	currentMsg := m.Message
-	if shouldRefetch {
-		time.Sleep(1500 * time.Millisecond)
-		refetchedMessage, err := s.ChannelMessage(m.ChannelID, m.ID)
-		if err != nil {
-			slog.Error("Error refetching the channel message while retrieving embeds", "error", err.Error())
-		} else {
-			currentMsg = refetchedMessage
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+	// Check if we have a pending batch of messages to send
+	buf, exists := g.MessageBuffer[m.ChannelID]
+	if !exists {
+		buf = MessageBuffer{
+			BufferedMessages: []*discordgo.Message{m.Message},
 		}
-	}
-
-	params := openai.ChatCompletionNewParams{
-		// https://openrouter.ai/docs/quickstart
-		Model:               g.Config.AIApiModels[0], // TODO allow fallbacks / retries,
-		MaxCompletionTokens: openai.Int(int64(g.Config.AIMaxOutputTokens)),
-	}
-
-	var messages []openai.ChatCompletionMessageParamUnion
-
-	// Give it context of the messages before our current one
-	history, err := s.ChannelMessages(m.ChannelID, g.Config.AINumberOfMessagesInHistory, m.ID, "", "")
-
-	if err != nil {
-		slog.Error("Error fetching channel history", "error", err.Error())
-	}
-	messages = append(messages, openai.SystemMessage(g.Config.AISystemPrompt))
-
-	// Messages come newest first, so reverse it
-	for i := len(history) - 1; i >= 0; i-- {
-		msg := history[i]
-		isAssistant := msg.Author.ID == s.State.User.ID // our own messages
-
-		messages = append(messages, buildMessages(msg, isAssistant))
-	}
-
-	// Add our newest message to the end
-	messages = append(messages, buildMessages(currentMsg, false))
-
-	// Set the messages and send it
-	params.Messages = messages
-	response, err := g.AIClient.Chat.Completions.New(context.TODO(), params)
-
-	if err != nil {
-		slog.Error("Error ocurred generating response", "error", err.Error(), "message", m.Content)
+		buf.Timer = time.AfterFunc(1500*time.Millisecond, func() {
+			g.processBufferedMessages(s, m.ChannelID)
+		})
+	} else if len(buf.BufferedMessages) < maxBufferedMessages {
+		buf.BufferedMessages = append(buf.BufferedMessages, m.Message)
+		buf.Timer.Reset(1500 * time.Millisecond)
+	} else {
+		// buffer is full
+		buf.Timer.Stop()
+		// Add the one that just came in
+		buf.BufferedMessages = append(buf.BufferedMessages, m.Message)
+		g.MessageBuffer[m.ChannelID] = buf
+		go g.processBufferedMessages(s, m.ChannelID)
 		return
 	}
 
-	if len(response.Choices) == 0 || strings.Contains(strings.ToLower(response.Choices[0].Message.Content), "no_response") {
-		// TODO use tools instead
-		return
-	}
-
-	// Add channel typing indicator
-	s.ChannelTyping(m.ChannelID)
-	jitter := time.Duration(rand.IntN(2000)+100) * time.Millisecond
-	time.Sleep(jitter)
-
-	_, err = s.ChannelMessageSend(m.ChannelID, response.Choices[0].Message.Content)
-
-	if err != nil {
-		slog.Error("Error sending discord message", "response", response, "error", err.Error())
-	}
+	// Write the appended message back to the global buffer
+	g.MessageBuffer[m.ChannelID] = buf
 }
 
 func shouldRefetchForEmbeds(msg *discordgo.Message) bool {
@@ -194,4 +158,103 @@ func downloadFile(url string) ([]byte, error) {
 	}
 
 	return io.ReadAll(resp.Body)
+}
+
+func (g *Godis) processBufferedMessages(s *discordgo.Session, channelId string) {
+	// Check if we need to refetch for any embeds on any bufferred messages
+	// Sometimes messages don't have embeds populated right away
+	g.mutex.Lock()
+	bufferedMessages := g.MessageBuffer[channelId].BufferedMessages
+	// Delete all the buffered messages so the timer starts again
+	delete(g.MessageBuffer, channelId)
+	g.mutex.Unlock()
+
+	// if any messages have links in them with no embeds
+	needsRefetch := false
+	if slices.ContainsFunc(bufferedMessages, shouldRefetchForEmbeds) {
+		needsRefetch = true
+	}
+
+	if needsRefetch {
+		// Wait once for all to populate embeds
+		time.Sleep(1500 * time.Millisecond)
+		for i, msg := range bufferedMessages {
+			if !shouldRefetchForEmbeds(msg) {
+				continue
+			}
+
+			refetchedMessage, err := s.ChannelMessage(channelId, msg.ID)
+			if err != nil {
+				// Keep the message as is without the embeds
+				slog.Error("Error refetching the channel message while retrieving embeds", "error", err.Error())
+			} else {
+				bufferedMessages[i] = refetchedMessage
+			}
+		}
+	}
+
+	aiParams := openai.ChatCompletionNewParams{
+		// https://openrouter.ai/docs/quickstart
+		Model:               g.Config.AIApiModels[0], // TODO allow fallbacks / retries,
+		MaxCompletionTokens: openai.Int(int64(g.Config.AIMaxOutputTokens)),
+	}
+
+	var aiMessagesToSend []openai.ChatCompletionMessageParamUnion
+
+	// Now we start adding the messages, first the system prompt
+	aiMessagesToSend = append(aiMessagesToSend, openai.SystemMessage(g.Config.AISystemPrompt))
+
+	// Now we should add some context of message history
+	// We need to set the correct message on the history look back, these should already have embeds
+	// as they are older than our buffer
+	historyCount := max(0, g.Config.AINumberOfMessagesInHistory-len(bufferedMessages))
+	if historyCount > 0 {
+		// Give it context of the messages before our first bufferred one (oldest), capped at what the user put in
+		history, err := s.ChannelMessages(channelId, historyCount, bufferedMessages[0].ID, "", "")
+
+		if err != nil {
+			slog.Error("Error fetching channel history", "error", err.Error())
+			// This is fine, we just wont add any history
+		}
+
+		// Messages in discord history come newest first, so reverse it and add it to the AI call
+		for i := len(history) - 1; i >= 0; i-- {
+			msg := history[i]
+			isAssistant := msg.Author.ID == s.State.User.ID // our own messages
+			aiMessagesToSend = append(aiMessagesToSend, buildMessages(msg, isAssistant))
+		}
+	}
+
+	// Add our bufferred messages at the end, this includes our current message
+	for _, msg := range bufferedMessages {
+		isAssistant := msg.Author.ID == s.State.User.ID // our own messages
+		aiMessagesToSend = append(aiMessagesToSend, buildMessages(msg, isAssistant))
+	}
+
+	// Set the messages and send it
+	aiParams.Messages = aiMessagesToSend
+	response, err := g.AIClient.Chat.Completions.New(context.TODO(), aiParams)
+
+	if err != nil {
+		slog.Error("Error ocurred generating response", "error", err.Error(), "message", bufferedMessages[len(bufferedMessages)-1])
+		return
+		// TODO dependng on if the model failed or whatever it might be good to send
+		// an appropriate response. right now just log
+	}
+
+	if len(response.Choices) == 0 || strings.Contains(strings.ToLower(response.Choices[0].Message.Content), "no_response") {
+		// TODO use tools instead
+		return
+	}
+
+	// Add channel typing indicator
+	s.ChannelTyping(channelId)
+	jitter := time.Duration(rand.IntN(2000)+100) * time.Millisecond
+	time.Sleep(jitter)
+
+	_, err = s.ChannelMessageSend(channelId, response.Choices[0].Message.Content)
+
+	if err != nil {
+		slog.Error("Error sending discord message", "response", response, "error", err.Error())
+	}
 }
